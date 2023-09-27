f4920844ad6dedf3a3424f283420cddece740bb9
Spekushka <143335767+Spekushka@users.noreply.github.com>
2023-09-27 16:47:17 +0300

    ping-pong.py v1.2
    теперь есть пончик, и хомячки бегают

--- ping-pong.py
+++ ping-pong.py
@@ -8,7 +8,7 @@ window = display.set_mode(
 )
 display.set_caption('piiiing-poooong')
 background = transform.scale(
-    image.load('images.jpg'),
+    image.load('bg.jpg'),
     (win_width, win_height)
 )
 
@@ -23,16 +23,33 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 class Player(GameSprite):
-    def update(self):
+    def updatel(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
+    def updater(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+ball = GameSprite('donut228.png', 100, 100, 10, 65, 70)
+homa1 = Player('homak.jpg', 0, 200, 10, 65, 70)
+homa2 = Player('homak.jpg', 630, 300, 10, 65, 70)
 clock = time.Clock()
 FPS = 60
 run = True
 while run:
-    window.blit(background, (0, 0))
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+    ball.reset()
+    homa1.updater()
+    homa1.reset()
+    homa2.updatel()
+    homa2.reset()
     display.update()
+    window.blit(background, (0, 0))
     clock.tick(FPS)
